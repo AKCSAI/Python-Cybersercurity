a0555c92ab55dd6f5ff79c8d068017219449b638
Aziz Khan <manruption@gmail.com>
2024-09-17 11:41:17 -0400

    This is Libraries commit

---
+++ Libraries.py
@@ -0,0 +1,6 @@
+#The re module, which provides functions used for searching for patterns in log files
+#The csv module, which provides functions used when working with .csv files
+#The glob and os modules, which provide functions used when interacting with the command line
+#The time and datetime modules, which provide functions used when working with timestamps
+
+import bs4
