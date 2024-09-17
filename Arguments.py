78847b1773376c0753e2917aafcc3be4cb2c9426
Aziz Khan <manruption@gmail.com>
2024-09-17 09:14:40 -0400

    This is an arguments commit

---
+++ Arguments.py
@@ -0,0 +1,8 @@
+print (type("hello"))
+
+#Explore input and output of print function 
+print ("This is a string, but", 75, "is a number.")
+
+#Explore input and output of type 
+print (type("security"))
+print (type(73.2))
