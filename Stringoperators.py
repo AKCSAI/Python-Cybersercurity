aa9302377f8d0da97049f6bc3ec5dcbc6c295975
Aziz Khan <manruption@gmail.com>
2024-09-18 12:30:42 -0400

    This is a first commit for lists.

--- Stringoperators.py
+++ Stringoperators.py
@@ -31,3 +31,59 @@ if device_id_lenght == 7:
     print ("The device has 7 characters.")
 else: 
     print ("all is good")
+
+
+
+# Check if the employee id meets length requirements 
+ 
+# Assign `employee_id` to a four digit number as an initial value
+employee_id = 4186
+
+# Reassign `employee_id` to the same value but in the form of a string
+employee_id = str(employee_id)
+
+# Conditional statement that displays a message if the length of `employee_id` is less than five digits
+if len(employee_id) < 5: 
+    print("This employee ID has less than five digits. It does not meet length requirements.")
+
+
+# Assign `employee_id` to a four digit number as an initial value
+employee_id = 4186
+
+# Reassign `employee_id` to the same value but in the form of a string
+employee_id = str(employee_id)
+
+# Display the `employee_id` as it currently stands
+print(employee_id, "is not valid.")
+
+# Conditional statement that updates the `employee_id` if its length is less than 5 digits
+if len(employee_id) < 5: 
+    employee_id = "E" + employee_id
+    
+# Display the `employee_id` after the update
+print (employee_id)
+
+
+# Assign `device_id` to a string that contains alphanumeric characters
+device_id = "r262c36"
+
+# Extract the fourth character in `device_id` and display it
+print(device_id[4])
+
+# Extract the first through the third characters in `device_id` and display the result
+print(device_id[0:3])
+
+# Assign `url` to a specific URL
+url = "https://exampleURL1.com"
+
+# Extract the protocol of `url` along with the syntax following it, display the result
+print (url[0:8])
+
+# Display the index where the domain extension ".com" is located in `url`
+print(url.index(".com"))
+
+# Extract the website name in `url` and display it
+print (url[8:ind])
+
+# Extract the domain extension in `url` and display it
+print(url[ind:ind+4])
