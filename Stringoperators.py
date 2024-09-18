7f42ec4c7a4881c24dc142ab238d25abbd088fb2
Aziz Khan <manruption@gmail.com>
2024-09-18 10:19:58 -0400

    This is the full commit for string operators

--- Stringoperators.py
+++ Stringoperators.py
@@ -15,3 +15,19 @@ print ("hello".lower())
 "Hello"[1]
 "Hello"[0:3]
 
+# Use the index string method 
+print ("HELLO".index("E"))
+print ("HELLO".index("L"))
+
+# Bracket notation refers to the indices placed in square brackets. 
+# You can use bracket notation to extract a part of a string. 
+device_id = "h32rb17"
+print("h32rb17"[0])
+print(device_id[0])
+
+#Identifying the length and 
+device_id_lenght = len("h32rb17")
+if device_id_lenght = 7 
+    print ("The device has 7 characters.")
+    
+
